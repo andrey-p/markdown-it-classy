ed4a6a71aa2736118449b168df5951fc26812968
Andrey Pissantchev <pissantchev@gmail.com>
2015-03-10 11:52:41 +0000

    rebuilt dist version

--- dist/markdown-it-classy.js
+++ dist/markdown-it-classy.js
@@ -6,6 +6,7 @@ var classy,
     { pattern: "p", fullName: "paragraph" },
     { pattern: "h\\d", fullName: "heading" },
     { pattern: "ul", fullName: "bullet_list" },
+    { pattern: "li", fullName: "list_item" },
     { pattern: "ol", fullName: "ordered_list" },
     { pattern: "blockquote", fullName: "blockquote" },
     { pattern: "em", fullName: "em", inline: true },
@@ -24,21 +25,17 @@ function isValidClassChar(code) {
 
 function parse(state) {
   var pos = state.pos,
-    posMax = state.posMax,
     classString = "";
 
   if (state.src.charCodeAt(pos) !== 0x7B) { // {
     return false;
   }
 
-  if (state.src.charCodeAt(posMax - 1) !== 0x7D) { // }
-    return false;
-  }
-
   // advance to account for opening brace
   pos += 1;
 
-  while (pos < posMax - 1) {
+  // grab everything til the closing brace
+  while (state.src.charCodeAt(pos) !== 0x7D) { // }
     if (!isValidClassChar(state.src.charCodeAt(pos))) {
       return false;
     }
@@ -47,7 +44,16 @@ function parse(state) {
     pos += 1;
   }
 
-  state.pos = posMax;
+  // advance to account for closing brace
+  pos += 1;
+
+  // only check classy statements at the end of the element
+  // or just before a newline
+  if (pos !== state.posMax && state.src.charCodeAt(pos) !== 0xA) { // \n
+    return false;
+  }
+
+  state.pos = pos;
 
   state.push({
     type: "classy",
@@ -60,7 +66,7 @@ function parse(state) {
 
 function getClassyFromBlockElement(tokens, idx, fullName) {
   var classy,
-    lastToken,
+    secondToLastToken,
     i,
     inlineContents;
 
@@ -85,23 +91,35 @@ function getClassyFromBlockElement(tokens, idx, fullName) {
     return null;
   }
 
-  // if the last token of the inline content is of type "classy"
-  // we have to do our thing
+  // only proceed if the last token of the inline content
+  // is of type "classy"
   if (inlineContents[inlineContents.length - 1].type !== "classy") {
     return null;
   }
 
-  classy = inlineContents.pop();
+  // this is where things get hairy
 
-  // might be some cleaning up to do...
-  lastToken = inlineContents[inlineContents.length - 1];
-  if (lastToken.type === "softbreak") {
-    // if the class was added via a newline,
-    // there'll be a rampant \n left
+  // we know that the last token is classy, but we also need to know
+  // whether classy was separated from the inline text by a newline or space
+  // so grab the second to last token...
+  secondToLastToken = inlineContents[inlineContents.length - 2];
+
+  if (secondToLastToken.type === "softbreak") {
+    // ... if it's split with a newline, we always apply the class
+    // to the containing block element
+    // so remove it from the inline contents
+    classy = inlineContents.pop();
+
+    // get rid of the soft break token too
     inlineContents.pop();
-  } else if (lastToken.type === "text") {
-    // else there might be some whitespace to trim
-    lastToken.content = lastToken.content.trim();
+  } else if (secondToLastToken.type === "text"
+      && ["blockquote", "ordered_list", "bullet_list"].indexOf(fullName) === -1) {
+    // ... if it wasn't a newline, and container isn't a blockquote,
+    // ol, or ul, apply this the class to it
+    classy = inlineContents.pop();
+
+    // trim whitespace of that last bit of text
+    secondToLastToken.content = secondToLastToken.content.trim();
   }
 
   return classy;
