c259e62c8e387b52201c372c865c3d9d5ae115e0
Andrey Pissantchev <pissantchev@gmail.com>
2015-05-21 12:23:51 +0100

    improved readability of character checking

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 "use strict";
 
-var classy;
+var classy,
+
+  // magic char codes
+  CHAR_0 = 0x30,
+  CHAR_9 = 0x39,
+  CHAR_A_UPPER = 0x41,
+  CHAR_Z_UPPER = 0x5A,
+  CHAR_A_LOWER = 0x61,
+  CHAR_Z_LOWER = 0x7A,
+  CHAR_UNDERSCORE = 0x5F,
+  CHAR_DASH = 0x2D,
+  CHAR_SPACE = 0x20,
+  CHAR_OPEN_CURLY = 0x7B,
+  CHAR_CLOSE_CURLY = 0x7D,
+  CHAR_NEWLINE = 0xA;
 
 function isValidClassChar(code) {
-  return (code >= 0x30 && code <= 0x39) || // 0-9
-    (code >= 0x41 && code <= 0x5A) || // A-Z
-    (code >= 0x61 && code <= 0x7A) || // a-z
-    code === 0x5F || // _
-    code === 0x2D || // -
-    code === 0x20; //   <- space
+  return (code >= CHAR_0 && code <= CHAR_9) ||
+    (code >= CHAR_A_UPPER && code <= CHAR_Z_UPPER) ||
+    (code >= CHAR_A_LOWER && code <= CHAR_Z_LOWER) ||
+    code === CHAR_UNDERSCORE ||
+    code === CHAR_DASH ||
+    code === CHAR_SPACE;
 }
 
 function parse(state) {
@@ -20,7 +34,7 @@ function parse(state) {
     preferOuter = false,
     token;
 
-  if (state.src.charCodeAt(pos) !== 0x7B) { // {
+  if (state.src.charCodeAt(pos) !== CHAR_OPEN_CURLY) {
     return false;
   }
 
@@ -28,7 +42,7 @@ function parse(state) {
   pos += 1;
 
   // grab everything til the closing brace
-  while (state.src.charCodeAt(pos) !== 0x7D) { // }
+  while (state.src.charCodeAt(pos) !== CHAR_CLOSE_CURLY) {
     if (!isValidClassChar(state.src.charCodeAt(pos))) {
       return false;
     }
@@ -40,13 +54,17 @@ function parse(state) {
   // advance to account for closing brace
   pos += 1;
 
-
-  // only check classy statements at the end of the element
-  if (pos !== state.posMax && state.src.charCodeAt(pos) !== 0xA) {
+  // only count curly brackets as a classy token if
+  // - at the end of the element
+  // - just before a newline
+  if (pos !== state.posMax && state.src.charCodeAt(pos) !== CHAR_NEWLINE) {
     return false;
   }
 
-  if (state.src.charCodeAt(initialPos - 1) === 0xA) {
+  // `preferOuter` keeps track of whether, in an ambiguous case
+  // (for instance with <ul>s and <li>s)
+  // we should prefer to add it on the containing element
+  if (state.src.charCodeAt(initialPos - 1) === CHAR_NEWLINE) {
     preferOuter = true;
   }
 
@@ -61,7 +79,7 @@ function parse(state) {
 
       // there might be a leftover space at the end
       pendingText = state.pending;
-      if (pendingText.charCodeAt(pendingText.length - 1) === 0x20) {
+      if (pendingText.charCodeAt(pendingText.length - 1) === CHAR_SPACE) {
         state.pending = pendingText.substring(0, pendingText.length - 1);
       }
 
@@ -94,6 +112,7 @@ function getClassyFromInlineToken(inlineToken) {
   classy = tokens.pop();
   numChildren -= 1;
 
+  // clean up after token was removed
   if (tokens[numChildren - 1].type === "softbreak") {
     // we may need to get rid of the newline just before classy statement
     tokens.pop(numChildren - 1);
